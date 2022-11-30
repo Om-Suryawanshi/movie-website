e88c873d043cfb5fb4aac3f5b3b05ab03f41f5a5
Om Suryawanshi <90911444+Om-Suryawanshi@users.noreply.github.com>
2022-11-30 18:59:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'api_key=2a90f92d8cd2ebc81c282097b3ba648b';
+const API_KEY = 'api_key=xxxxxxxxxxxxxxxxxxxxxxxxxx';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
